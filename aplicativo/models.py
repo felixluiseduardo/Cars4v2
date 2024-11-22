b1f28944960c14296f8659b1ad0a0041b9424f54
luis <felixluiseduardo717@gmail.com>
2024-11-22 16:10:02 -0300

    Usuario logado

--- aplicativo/models.py
+++ aplicativo/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-
+import django
 # Create your models here.
 class Car(models.Model):
     model = models.CharField(max_length=50, null=False)
     brand = models.CharField(max_length=50, null=False)
     year = models.IntegerField(null=False)
     filename = models.CharField(max_length=50, null=False)
+    user = models.ForeignKey(
+        django.contrib.auth.get_user_model(), 
+        on_delete=models.CASCADE
+        )
     
     def __str__(self):
         return self.model + ' ano: ' + str(self.year)
